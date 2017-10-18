f6dfb7c0f17eb8b24568e7c1cdd5464370e6b8e6
huangqiwei <dz2008hqw@163.com>
2017-10-18 10:44:33 +0800

    调整命名空间名称

--- HMQService/Common/Log.cs
+++ HMQService/Common/Log.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using log4net;
 
-namespace HMQService
+namespace HMQService.Common
 {
     public class Log
     {
--- HMQService/HMQService.cs
+++ HMQService/HMQService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.ServiceProcess;
+using HMQService.Common;
 
 namespace HMQService
 {
